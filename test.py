0a922a83ecfb078491d0672eac751a6ae28a6ed0
Chekuri P V S Yogananda Varma <110842297+premsaivarmachekuri@users.noreply.github.com>
2023-02-12 23:44:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import main;
 
 def test1():
-    assert main.average_calories([2200, 2500, 2700, 2900, "done"]) == 2600.0
+    assert main.average_calories([2200, 2500, 2700, 2900, "done"]) == 2575.0
 def test2():
     assert main.average_calories([200, 300, 100, "done"]) == 200.0
 def test3():
-    assert main.average_calories([1500.25, 1700.50, 1900.75, "done"]) == 1700.5
+    assert main.average_calories([1500.2, 1700.3, 1900.4, "done"]) == 1700.3
 
