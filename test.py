eb63dc258ffca64fc398cf7af2aebdd80b93d17b
Chekuri P V S Yogananda Varma <110842297+premsaivarmachekuri@users.noreply.github.com>
2023-02-12 23:24:15 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import main;
 
 def test1():
-    assert main.make_tuples([1,2,3,4], [5,6,7,8]) == [(1,5), (2,6), (3,7), (4,8)]
+    assert main.average_calories([2200, 2500, 2700, 2900, "done"]) == 2600.0
 def test2():
-    assert main.make_tuples([1,2.5,3,4], [5,6,7,8]) == [(1,5), (2.5,6), (3,7), (4,8)]
+    assert main.average_calories([200, 300, 100, "done"]) == 200.0
 def test3():
-    assert main.make_tuples(['a','b','c','d'], ['e','f','g','h']) == [('a','e'), ('b','f'), ('c','g'), ('d','h')]
+    assert main.average_calories([1500.25, 1700.50, 1900.75, "done"]) == 1700.50
+
